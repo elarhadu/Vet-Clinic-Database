c1cedf995ce41cde50b52e0b2a11c4b54803440a
elarhadu <elarhadu@gmail.com>
2023-02-24 15:16:02 +0000

    Add Transactions and update queries

--- queries.sql
+++ queries.sql
@@ -6,3 +6,21 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered= 'TRUE';
 SELECT * FROM animals WHERE NOT name='Gabumon';
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+-- TRANSACTIONS
+UPDATE animals SET species='digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species='pokemon' WHERE species IS NULL;
+DELETE FROM animals;
+DELETE FROM animals WHERE date_of_birth>'2022-01-01';
+SAVEPOINT SPGABUMON;
+UPDATE animals SET weight_kg=weight_kg * -1;
+UPDATE animals SET weight_kg=weight_kg * -1 WHERE weight_kg<0;
+
+-- QUERIES
+SELECT COUNT(*) FROM animals;
+SELECT COUNT(*) FROM animals WHERE escape_attempts=0;
+SELECT AVG(weight_kg) FROM animals;
+SELECT neutered, AVG(escape_attempts) AS avg_escape_attempts FROM animals GROUP BY neutered;
+SELECT species, MIN(weight_kg) AS min_weight_kg, MAX(weight_kg) AS max_weight_kg FROM animals GROUP BY species;
+SELECT species, AVG(escape_attempts) AS avg_escape_attempts FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
+
